b0c08207b777b15744d35a58abad9aa792b1670c
edison681 <67926452+edison681@users.noreply.github.com>
2022-10-06 16:25:55 -1000

    Add files via upload

---
+++ nft_sandbox.js
@@ -0,0 +1,397 @@
+// xmana_nft_smart_array = nft_id+"@"+staking_address+"#"+staking_side+"$"+token_amount
+var xmana_nfts_smart_array = [];
+
+//***************************
+//** Mint Token *************
+//***************************
+
+const server_address = "wss://xls20-sandbox.rippletest.net:51233";
+
+async function mintToken() {
+  const staked_token = 10;
+  const wallet = xrpl.Wallet.fromSeed(secret.value);
+  const client = new xrpl.Client(server_address);
+  await client.connect();
+  console.log("Connected to Sandbox");
+
+  //check balance that wallet has enough token to stake/escrow the required tokens for minting
+  if (client.getXrpBalance(wallet.address) < staked_token + 20) {
+    console.log("Not enough token to stake your nft minting");
+    return;
+  } else {
+    // Call Xmana Smart/Escrow function to stake required tokens before minting NFT
+    Smart_Escrow.createEscrow(wallet, staked_token);
+
+    // Note that you must convert the token URL to a hexadecimal
+    // value for this transaction.
+    // ----------------------------------------------------------
+    const transactionBlob = {
+      TransactionType: "NFTokenMint",
+      Account: wallet.classicAddress,
+      URI: xrpl.convertStringToHex(tokenUrl.value),
+      Flags: parseInt(flags.value),
+      NFTokenTaxon: 0, //Required, but if you have no use for it, set to zero.
+    };
+    //******* nft_id = transactionBlob.nft_id
+    // Submit signed blob --------------------------------------------------------
+    const tx = await client.submitAndWait(transactionBlob, { wallet });
+
+    xmana_nfts_smart_array.push(
+      tx.NFTokenID.value +
+        "@" +
+        wallet.classicAddress +
+        "#" +
+        "VouchIt" +
+        staked_token.toString
+    );
+
+    const nfts = await client.request({
+      method: "account_nfts",
+      account: wallet.classicAddress,
+    });
+    console.log(nfts);
+    // Check transaction results -------------------------------------------------
+    console.log("Transaction result:", tx.result.meta.TransactionResult);
+    console.log(
+      "Balance changes:",
+      JSON.stringify(xrpl.getBalanceChanges(tx.result.meta), null, 2)
+    );
+    client.disconnect();
+  }
+} //End of mintToken
+
+//***************************
+//** Get Tokens *************
+//***************************
+
+async function getTokens() {
+  const wallet = xrpl.Wallet.fromSeed(secret.value);
+  const client = new xrpl.Client(server_address);
+  await client.connect();
+  console.log("Connected to Sandbox");
+  const nfts = await client.request({
+    method: "account_nfts",
+    account: wallet.classicAddress,
+  });
+  console.log(nfts);
+  client.disconnect();
+} //End of getTokens
+
+//***************************
+//** Burn Token *************
+//***************************
+
+async function burnToken() {
+  const wallet = xrpl.Wallet.fromSeed(secret.value);
+  const client = new xrpl.Client(server_address);
+  await client.connect();
+  console.log("Connected to Sandbox");
+
+  // Prepare transaction -------------------------------------------------------
+  const transactionBlob = {
+    TransactionType: "NFTokenBurn",
+    Account: wallet.classicAddress,
+    NFTokenID: tokenId.value,
+  };
+
+  // Submit signed blob --------------------------------------------------------
+  const tx = await client.submitAndWait(transactionBlob, { wallet });
+  const nfts = await client.request({
+    method: "account_nfts",
+    account: wallet.classicAddress,
+  });
+  console.log(nfts);
+  // Check transaction results -------------------------------------------------
+  console.log("Transaction result:", tx.result.meta.TransactionResult);
+  console.log(
+    "Balance changes:",
+    JSON.stringify(xrpl.getBalanceChanges(tx.result.meta), null, 2)
+  );
+  client.disconnect();
+}
+// End of burnToken()
+
+//********************************
+//** Create Sell Offer ***********
+//********************************
+
+async function createSellOffer() {
+  const wallet = xrpl.Wallet.fromSeed(secret.value);
+  const client = new xrpl.Client(server_address);
+  await client.connect();
+  console.log("Connected to Sandbox");
+
+  // Prepare transaction -------------------------------------------------------
+  const transactionBlob = {
+    TransactionType: "NFTokenCreateOffer",
+    Account: wallet.classicAddress,
+    NFTokenID: tokenId.value,
+    Amount: amount.value,
+    Flags: parseInt(flags.value),
+  };
+
+  // Submit signed blob --------------------------------------------------------
+
+  const tx = await client.submitAndWait(transactionBlob, { wallet }); //AndWait
+
+  console.log("***Sell Offers***");
+  let nftSellOffers;
+  try {
+    nftSellOffers = await client.request({
+      method: "nft_sell_offers",
+      nft_id: tokenId.value,
+    });
+  } catch (err) {
+    console.log("No sell offers.");
+  }
+  console.log(JSON.stringify(nftSellOffers, null, 2));
+  console.log("***Buy Offers***");
+  let nftBuyOffers;
+  try {
+    nftBuyOffers = await client.request({
+      method: "nft_buy_offers",
+      nft_id: tokenId.value,
+    });
+  } catch (err) {
+    console.log("No buy offers.");
+  }
+  console.log(JSON.stringify(nftBuyOffers, null, 2));
+
+  // Check transaction results -------------------------------------------------
+  console.log(
+    "Transaction result:",
+    JSON.stringify(tx.result.meta.TransactionResult, null, 2)
+  );
+  console.log(
+    "Balance changes:",
+    JSON.stringify(xrpl.getBalanceChanges(tx.result.meta), null, 2)
+  );
+  client.disconnect();
+  // End of createSellOffer()
+}
+//********************************
+//** Create Buy Offer ***********
+//********************************
+
+async function createBuyOffer() {
+  const wallet = xrpl.Wallet.fromSeed(secret.value);
+  const client = new xrpl.Client(server_address);
+  await client.connect();
+  console.log("Connected to Sandbox");
+
+  // Prepare transaction -------------------------------------------------------
+  const transactionBlob = {
+    TransactionType: "NFTokenCreateOffer",
+    Account: wallet.classicAddress,
+    Owner: owner.value,
+    NFTokenID: tokenId.value,
+    Amount: amount.value,
+    Flags: parseInt(flags.value),
+  };
+
+  // Submit signed blob --------------------------------------------------------
+  const tx = await client.submitAndWait(transactionBlob, { wallet });
+
+  console.log("***Sell Offers***");
+  let nftSellOffers;
+  try {
+    nftSellOffers = await client.request({
+      method: "nft_sell_offers",
+      nft_id: tokenId.value,
+    });
+  } catch (err) {
+    console.log("No sell offers.");
+  }
+  console.log(JSON.stringify(nftSellOffers, null, 2));
+  console.log("***Buy Offers***");
+  let nftBuyOffers;
+  try {
+    nftBuyOffers = await client.request({
+      method: "nft_buy_offers",
+      nft_id: tokenId.value,
+    });
+  } catch (err) {
+    console.log("No buy offers.");
+  }
+  console.log(JSON.stringify(nftBuyOffers, null, 2));
+
+  // Check transaction results -------------------------------------------------
+  console.log(
+    "Transaction result:",
+    JSON.stringify(tx.result.meta.TransactionResult, null, 2)
+  );
+  console.log(
+    "Balance changes:",
+    JSON.stringify(xrpl.getBalanceChanges(tx.result.meta), null, 2)
+  );
+  client.disconnect();
+  // End of createBuyOffer()
+}
+
+//***************************
+//** Cancel Offer ***********
+//***************************
+
+async function cancelOffer() {
+  const wallet = xrpl.Wallet.fromSeed(secret.value);
+  const client = new xrpl.Client(server_address);
+  await client.connect();
+  console.log("Connected to Sandbox");
+
+  const tokenOfferID = tokenOfferIndex.value;
+  const tokenOffers = [tokenOfferID];
+
+  // Prepare transaction -------------------------------------------------------
+  const transactionBlob = {
+    TransactionType: "NFTokenCancelOffer",
+    Account: wallet.classicAddress,
+    NFTokenOffers: tokenOffers,
+  };
+
+  // Submit signed blob --------------------------------------------------------
+  const tx = await client.submitAndWait(transactionBlob, { wallet });
+
+  console.log("***Sell Offers***");
+  let nftSellOffers;
+  try {
+    nftSellOffers = await client.request({
+      method: "nft_sell_offers",
+      nft_id: tokenId.value,
+    });
+  } catch (err) {
+    console.log("No sell offers.");
+  }
+  console.log(JSON.stringify(nftSellOffers, null, 2));
+  console.log("***Buy Offers***");
+  let nftBuyOffers;
+  try {
+    nftBuyOffers = await client.request({
+      method: "nft_buy_offers",
+      nft_id: tokenId.value,
+    });
+  } catch (err) {
+    console.log("No buy offers.");
+  }
+  console.log(JSON.stringify(nftBuyOffers, null, 2));
+
+  // Check transaction results -------------------------------------------------
+
+  console.log(
+    "Transaction result:",
+    JSON.stringify(tx.result.meta.TransactionResult, null, 2)
+  );
+  console.log(
+    "Balance changes:",
+    JSON.stringify(xrpl.getBalanceChanges(tx.result.meta), null, 2)
+  );
+
+  client.disconnect();
+  // End of cancelOffer()
+}
+//***************************
+//** Get Offers *************
+//***************************
+
+async function getOffers() {
+  const wallet = xrpl.Wallet.fromSeed(secret.value);
+  const client = new xrpl.Client(server_address);
+  await client.connect();
+  console.log("Connected to Sandbox");
+  console.log("***Sell Offers***");
+
+  let nftSellOffers;
+  try {
+    nftSellOffers = await client.request({
+      method: "nft_sell_offers",
+      nft_id: tokenId.value,
+    });
+  } catch (err) {
+    console.log("No sell offers.");
+  }
+  console.log(JSON.stringify(nftSellOffers, null, 2));
+  console.log("***Buy Offers***");
+  let nftBuyOffers;
+  try {
+    nftBuyOffers = await client.request({
+      method: "nft_buy_offers",
+      nft_id: tokenId.value,
+    });
+  } catch (err) {
+    console.log("No buy offers.");
+  }
+  console.log(JSON.stringify(nftBuyOffers, null, 2));
+  client.disconnect();
+  // End of getOffers()
+}
+//***************************
+//** Accept Sell Offer ******
+//***************************
+
+async function acceptSellOffer() {
+  const wallet = xrpl.Wallet.fromSeed(secret.value);
+  const client = new xrpl.Client(server_address);
+  await client.connect();
+  console.log("Connected to Sandbox");
+
+  // Prepare transaction -------------------------------------------------------
+  const transactionBlob = {
+    TransactionType: "NFTokenAcceptOffer",
+    Account: wallet.classicAddress,
+    NFTokenSellOffer: tokenOfferIndex.value,
+  };
+  // Submit signed blob --------------------------------------------------------
+  const tx = await client.submitAndWait(transactionBlob, { wallet });
+  const nfts = await client.request({
+    method: "account_nfts",
+    account: wallet.classicAddress,
+  });
+  console.log(JSON.stringify(nfts, null, 2));
+
+  // Check transaction results -------------------------------------------------
+  console.log(
+    "Transaction result:",
+    JSON.stringify(tx.result.meta.TransactionResult, null, 2)
+  );
+  console.log(
+    "Balance changes:",
+    JSON.stringify(xrpl.getBalanceChanges(tx.result.meta), null, 2)
+  );
+  client.disconnect();
+  // End of acceptSellOffer()
+}
+//***************************
+//** Accept Buy Offer ******
+//***************************
+
+async function acceptBuyOffer() {
+  const wallet = xrpl.Wallet.fromSeed(secret.value);
+  const client = new xrpl.Client(server_address);
+  await client.connect();
+  console.log("Connected to Sandbox");
+
+  // Prepare transaction -------------------------------------------------------
+  const transactionBlob = {
+    TransactionType: "NFTokenAcceptOffer",
+    Account: wallet.classicAddress,
+    NFTokenBuyOffer: tokenOfferIndex.value,
+  };
+  // Submit signed blob --------------------------------------------------------
+  const tx = await client.submitAndWait(transactionBlob, { wallet });
+  const nfts = await client.request({
+    method: "account_nfts",
+    account: wallet.classicAddress,
+  });
+  console.log(JSON.stringify(nfts, null, 2));
+
+  // Check transaction results -------------------------------------------------
+  console.log(
+    "Transaction result:",
+    JSON.stringify(tx.result.meta.TransactionResult, null, 2)
+  );
+  console.log(
+    "Balance changes:",
+    JSON.stringify(xrpl.getBalanceChanges(tx.result.meta), null, 2)
+  );
+  client.disconnect();
+  // End of submitTransaction()
+}
---
+++ smart_sandbox.js
@@ -0,0 +1,222 @@
+// *******************************************************
+// * Xmana Smart Contracts for all actions on the Xmana Platform *
+// * Based on XRPL Escrow functionality*
+// * For concept demonstration only, final product/code will need to modify the XRPL library directly *
+// * Be transformed to the needs of Xmana platform - high volume, low value, staking mechanism, multiple *
+// * Beneficiaries which are not known by the time when escrow was created, etc. *
+const xmana_smart_dist_adr = "rnuvEZ8yyNNe8j4wk9rfhdzrKv5AdvW2gk";
+const xmana_smart_dist_seed = "sEd7ZKS7g1kMxs1afu9txCZ2NLAGQkD";
+const xmana_smart_dist_wallet = xrpl.Wallet.fromSeed(xmana_smart_dist_seed);
+const xmana_nft_stake_threshold = 0.1;
+
+class Smart_Escrow {
+  constructor(wallet, staked_amount) {
+    this.wallet = wallet;
+    this.staked_amount = staked_amount;
+  }
+  // *******************************************************
+  // ************* Create Escrow ***************************
+  // *******************************************************
+  static createEscrow(wallet, staked_amount) {
+    ("use strict");
+    const RippleAPI = require("ripple-lib").RippleAPI;
+    const cc = require("five-bells-condition");
+    const crypto = require("crypto");
+
+    const myAddr = wallet.classicAddress;
+    const mySecret = wallet.seed;
+
+    // Construct condition and fulfillment
+    const preimageData = crypto.randomBytes(32);
+    const myFulfillment = new cc.PreimageSha256();
+    myFulfillment.setPreimage(preimageData);
+    const conditionHex = myFulfillment
+      .getConditionBinary()
+      .toString("hex")
+      .toUpperCase();
+
+    console.log("Condition:", conditionHex);
+    console.log(
+      "Fulfillment:",
+      myFulfillment.serializeBinary().toString("hex").toUpperCase()
+    );
+
+    // Construct transaction
+    const currentTime = new Date();
+    const myEscrow = {
+      destination: xmana_smart_dist_wallet.classicAddress, // Destination can be same as source
+      destinationTag: 2017,
+      amount: staked_amount, //decimal XRP
+      condition: conditionHex,
+      allowExecuteAfter: currentTime.toISOString(), // can be executed right away if the condition is met
+    };
+    const myInstructions = {
+      maxLedgerVersionOffset: 5,
+    };
+
+    // Connect and submit
+    const api = new RippleAPI({ server: "wss://s2.ripple.com" });
+
+    function submitTransaction(lastClosedLedgerVersion, prepared, secret) {
+      const signedData = api.sign(prepared.txJSON, secret);
+      console.log("Transaction ID: ", signedData.id);
+      return api.submit(signedData.signedTransaction).then((data) => {
+        console.log("Tentative Result: ", data.resultCode);
+        console.log("Tentative Message: ", data.resultMessage);
+      });
+    }
+
+    api
+      .connect()
+      .then(() => {
+        console.log("Connected");
+        return api.prepareEscrowCreation(myAddr, myEscrow, myInstructions);
+      })
+      .then((prepared) => {
+        console.log("EscrowCreation Prepared");
+        return api.getLedger().then((ledger) => {
+          console.log("Current Ledger", ledger.ledgerVersion);
+          return submitTransaction(ledger.ledgerVersion, prepared, mySecret);
+        });
+      })
+      .then(() => {
+        api.disconnect().then(() => {
+          console.log("api disconnected");
+          process.exit();
+        });
+      })
+      .catch(console.error);
+  }
+  // *******************************************************
+  // ************* Cancel Escrow ***************************
+  // *******************************************************
+  static cancelEscrow(wallet) {
+    ("use strict");
+    const RippleAPI = require("ripple-lib").RippleAPI;
+
+    const myAddr = wallet.classicAddress;
+    const mySecret = wallet.seed;
+
+    const myEscrowCancellation = {
+      owner: myAddr,
+      escrowSequence: 366,
+    };
+    const myInstructions = {
+      maxLedgerVersionOffset: 5,
+    };
+
+    const api = new RippleAPI({ server: "wss://s2.ripple.com" });
+
+    function submitTransaction(lastClosedLedgerVersion, prepared, secret) {
+      const signedData = api.sign(prepared.txJSON, secret);
+      console.log("Transaction ID: ", signedData.id);
+      return api.submit(signedData.signedTransaction).then((data) => {
+        console.log("Tentative Result: ", data.resultCode);
+        console.log("Tentative Message: ", data.resultMessage);
+      });
+    }
+
+    api
+      .connect()
+      .then(() => {
+        console.log("Connected");
+        return api.prepareEscrowCancellation(
+          myAddr,
+          myEscrowCancellation,
+          myInstructions
+        );
+      })
+      .then((prepared) => {
+        console.log("EscrowCancellation Prepared");
+        return api.getLedger().then((ledger) => {
+          console.log("Current Ledger", ledger.ledgerVersion);
+          return submitTransaction(ledger.ledgerVersion, prepared, mySecret);
+        });
+      })
+      .then(() => {
+        api.disconnect().then(() => {
+          console.log("api disconnected");
+          process.exit();
+        });
+      })
+      .catch(console.error);
+  }
+  // *******************************************************
+  // ************* Make Condition **************************
+  // *******************************************************
+  static makeCondition() {
+    const cc = require("five-bells-condition");
+    const crypto = require("crypto");
+
+    const preimageData = crypto.randomBytes(32);
+    const myFulfillment = new cc.PreimageSha256();
+    myFulfillment.setPreimage(preimageData);
+
+    console.log(
+      "Condition:",
+      myFulfillment.getConditionBinary().toString("hex").toUpperCase()
+    );
+    console.log(
+      "Fulfillment:",
+      myFulfillment.serializeBinary().toString("hex").toUpperCase()
+    );
+  }
+  // *******************************************************
+  // ************ Xmana Smart Distribution Bot *************
+  // *******************************************************
+        
+// xmaNFT_smtArr = nft_id+"@"+staking_address+"#"+staking_side+"$"+token_amount
+  static xmana_smart_dist_bot(xmaNFT_smtArr){
+    var total_vouch
+    var total_beat
+        for (let i = 0; i < xmaNFT_smtArr.length; i++) {
+            if (xmaNFT_smtArr[i].slice(xmaNFT_smtArr[i].indexOf('#') + 1, xmaNFT_smtArr[i].lastIndexOf('$')) = "VouchIt") { 
+                total_vouch = total_vouch + parsefloat(xmaNFT_smtArr[i].slice(xmaNFT_smtArr[i].indexOf('$') + 1)) }
+            else if (xmaNFT_smtArr[i].slice(xmaNFT_smtArr[i].indexOf('#') + 1,xmaNFT_smtArr[i].lastIndexOf('$')) = "BeatIt") {
+                total_beat = total_beat + parsefloat(xmaNFT_smtArr[i].slice(xmaNFT_smtArr[i].indexOf('$') + 1))
+                }
+        }
+        //connect xmana_smart_dist_wallet to make distribution
+          const client = new xrpl.Client(server_address)
+          client.connect()
+        //loop to distribute the tokens
+        for (let i = 0; i < xmaNFT_smtArr.length; i++) { 
+        // total_beat side won, will receive total_vouch side tokens based on stake weight
+                if (total_vouch / total_beat < xmana_nft_stake_threshold) {
+                        if (xmaNFT_smtArr[i].slice(xmaNFT_smtArr[i].indexOf('#') + 1, xmaNFT_smtArr[i].lastIndexOf('$')) = "BeatIt") {
+                                var temp_recAdr = xmaNFT_smtArr[i].slice(xmaNFT_smtArr[i].indexOf('@') + 1, xmaNFT_smtArr[i].lastIndexOf('#'))
+                                var temp_distAmt = total_vouch * (parsefloat(xmaNFT_smtArr[i].slice(xmaNFT_smtArr[i].indexOf('$') + 1)) / total_beat)
+                        }
+                }
+                // total_vouch side won, will receive total_beat side tokens based on stake weight        
+                else if (total_beat / total_vouch < xmana_nft_stake_threshold) {
+                        if (xmaNFT_smtArr[i].slice(xmaNFT_smtArr[i].indexOf('#') + 1, xmaNFT_smtArr[i].lastIndexOf('$')) = "VouchIt") {
+                                var temp_recAdr = xmaNFT_smtArr[i].slice(xmaNFT_smtArr[i].indexOf('@') + 1, xmaNFT_smtArr[i].lastIndexOf('#'))
+                                var temp_distAmt = total_beat * (parsefloat(xmaNFT_smtArr[i].slice(xmaNFT_smtArr[i].indexOf('$') + 1)) / total_vouch)
+                        }
+                }
+        // inconclusive staking result, all tokens will go back to original wallet                
+                else {
+
+
+
+                        
+                 }
+        // Prepare transaction
+          const prepared = client.autofill({
+          "TransactionType": "Payment",
+          "Account": xmana_smart_dist_wallet.address,
+          "Amount": temp_distAmt,
+          "Destination": temp_recAdr
+        })
+      
+        // ------------------------------------------------ Sign prepared instructions
+        const signed = standby_wallet.sign(prepared)
+      
+        // -------------------------------------------------------- Submit signed blob
+        const tx = await client.submitAndWait(signed.tx_blob)
+         }
+        
+          
+  }
+}
